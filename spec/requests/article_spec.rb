2c8c072a3ebcb13c9aa55d33fbde0580940b5745
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2017-06-13 23:46:27 +0000

    Restrict Access with controller edit/update action.

--- spec/requests/article_spec.rb
+++ spec/requests/article_spec.rb
@@ -3,7 +3,45 @@ require "rails_helper"
 RSpec.describe "Articles", type: :request do
   
   before do
-    @article = Article.create(title: "Test Title", body: "test Body")
+    @test_user = User.create(email: "user@test-mail.com", password: "password")
+    @fred = User.create(email: "fred@test-mail.com", password: "password")
+    @article = Article.create!(title: "Test Title", body: "test Body", user: @test_user)
+  end
+  
+  describe "GET /articles/:id/edit" do
+    context "with non-signed in user" do
+      before { get "/articles/#{@article.id}/edit"}
+      
+      it "redirects to the sign in page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+    
+    context "with signed in user who is non-owner" do
+      before do
+        login_as(@fred)
+        get "/articles/#{@article.id}/edit"
+      end
+      
+      it "redirects to home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only edit your own article."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+    
+    context "with signed in user successfull edit" do
+      before do
+        login_as(@test_user)
+        get "/articles/#{@article.id}/edit"
+      end
+      
+      it "successfuly edit article" do
+        expect(response.status).to eq 200
+      end
+    end
   end
   
   describe "GET /articles/:id" do
