d794d83aa4805d2916c292fa4e56928e36ed851e
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2017-06-14 16:00:59 +0000

    Adds spec for redirect if non-owner tries to delete article.

--- spec/requests/article_spec.rb
+++ spec/requests/article_spec.rb
@@ -38,8 +38,46 @@ RSpec.describe "Articles", type: :request do
         get "/articles/#{@article.id}/edit"
       end
       
-      it "successfuly edit article" do
+      it "successfully edit article" do
         expect(response.status).to eq 200
+      end
+    end
+  end
+  
+  describe "DELETE /articles/:id" do
+    context "with non-signed in user" do
+      before { delete "/articles/#{@article.id}"}
+      
+      it "redirects to the sign in page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+    
+    context "with signed in user who is non-owner" do
+      before do
+        login_as(@fred)
+        delete "/articles/#{@article.id}"
+      end
+      
+      it "redirects to home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only delete your own article."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+    
+    context "with signed in user successfull delete" do
+      before do
+        login_as(@test_user)
+        delete "/articles/#{@article.id}"
+      end
+      
+      it "successfully delete article" do
+        expect(response.status).to eq 302
+        flash_message = "Article has been deleted"
+        expect(flash[:success]).to eq flash_message
       end
     end
   end
