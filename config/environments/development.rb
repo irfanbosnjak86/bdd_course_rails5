500197cdcd665981e99fa115214802ddc72edcdd
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2017-06-18 23:05:38 +0000

    Adds realtime feature.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # This is for ActionCable server to monitor all ports(default is 3000)
+  config.action_cable.disable_request_forgery_protection = true
 end
